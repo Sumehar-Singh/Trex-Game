7a00a6a72332bbcda0b4d617421e1f22e682aa73
Sumehar Singh Grewal <76869140+Sumehar-Singh@users.noreply.github.com>
2021-01-05 21:40:17 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -65,7 +65,7 @@ function setup() {
   cloudsGroup = new Group();
   obstaclesGroup = new Group();
   
-  trex.setCollider("rectangle", 0, 0, 50, 80);
+  trex.setCollider("rectangle", 0, 0, 40, 80);
   
   score = 0;
 }
